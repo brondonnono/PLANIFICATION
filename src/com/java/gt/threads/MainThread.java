8e099db9f68318aa48a268a366e201c75e258903
arleon zemtsop <arleonzemtsop@outlook.fr>
2020-09-19 18:09:56 +0100

    Mise à jour du thread pour la gestion des sauvegardes

--- src/com/java/gt/threads/MainThread.java
+++ src/com/java/gt/threads/MainThread.java
@@ -1,7 +1,16 @@
 package com.java.gt.threads;
 
 import com.java.gt.controllers.store_controllers.StorageController;
+import com.java.gt.configurations.StorageConfig;;
 
+/**
+ * @author Arléon Zemtsop
+ * Ce module les opérations s'exécutant en parallèle
+ * au cours du fonctionnement du programme principal
+ * Il s'agit entre autre des opérations de sauvegarde 
+ * du temps d'exécution d'une tâche depuis sa dernière
+ * maintenance
+*/
 public class MainThread extends Thread {
     private StorageController storageController;
 
@@ -11,15 +20,18 @@ public class MainThread extends Thread {
         this.storageController = storageController;
     }
 
+    /**
+     * Cette fonction ajoute 2 seconde au temps de fonctionnement des tâches d'un équipement
+     */
     @Override
 	public void run() {
 		while(true) {
-            System.out.println("Daniela je t'aime :3 :3 :3 :3 !");
-            this.storageController.getFileWriter().renderOperatingTime(2, this.storageController.getFileReader().getTaskList());
+            // Ce bout de code sera exécuté toutes les 2 secondes
+            this.storageController.getFileWriter().renderOperatingTime(StorageConfig.DEFAULT_OPERATING_TIME_SAVE_DATA_FREQUENCY_IN_SECOND, this.storageController.getFileReader().getTaskList());
             this.storageController.getFileWriter().renderAllTasks(this.storageController.getFileReader().getTaskList());
             System.out.println(this.storageController.getFileReader().getTaskList());
 			try{
-                Thread.sleep(2000);
+                Thread.sleep(StorageConfig.DEFAULT_OPERATING_TIME_SAVE_DATA_FREQUENCY_IN_SECOND * 1000);
 			} catch(Exception e) {}
 		}
 	}
